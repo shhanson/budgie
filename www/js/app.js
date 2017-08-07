7530dda2ad10d4c2636ea62d1f35ce5b03e653a7
Amanda Allen <amanda.allen2488@gmail.com>
2017-08-07 17:27:05 -0500

    merge

--- www/js/app.js
+++ www/js/app.js
@@ -23,8 +23,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   // Learn more here: https://github.com/angular-ui/ui-router
   // Set up the various states which the app can be in.
   // Each state's controller can be found in controllers.js
-  $stateProvider
-
+  $stateProvider.state('splash', {
+    url: '',
+    templateUrl: 'templates/splash.html'
+  })
   // setup an abstract state for the tabs directive
     .state('tab', {
     url: '/tab',
@@ -76,5 +78,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   })
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/receipts');
+  $urlRouterProvider.otherwise('/');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', []).controller('ReceiptsCtrl', function($s
   $scope.receipts;
 
   $scope.getReceipts = function() {
-    $http.get('http://ec2-18-220-68-160.us-east-2.compute.amazonaws.com:8000/receipts/users/1').then((res) => {
+    $http.get('http://ec2-18-220-68-160.us-east-2.compute.amazonaws.com:8001/receipts/users/1').then((res) => {
       $scope.receipts = res.data;
     });
   };
@@ -21,13 +21,13 @@ angular.module('starter.controllers', []).controller('ReceiptsCtrl', function($s
   };
   $scope.closeModal = function() {
     $scope.modal.hide();
-  }
+  };
 
 }).controller('ItemsCtrl', function($scope, $stateParams, $http) {
   $scope.items;
 
   $scope.getItems = function() {
-    $http.get(`http://ec2-18-220-68-160.us-east-2.compute.amazonaws.com:8000/receipts/${$stateParams.receiptId}/items`).then((res) => {
+    $http.get(`http://ec2-18-220-68-160.us-east-2.compute.amazonaws.com:8001/receipts/${$stateParams.receiptId}/items`).then((res) => {
       $scope.items = res.data;
     });
   };
@@ -51,7 +51,7 @@ angular.module('starter.controllers', []).controller('ReceiptsCtrl', function($s
   $scope.settings = {
     enableFriends: true
   };
-}).controller('GraphCtrl', function($scope, $http) {
+}).controller('GraphCtrl', function($scope, $http, $ionicModal) {
   $scope.options = {
     chart: {
       type: 'discreteBarChart',
@@ -63,12 +63,13 @@ angular.module('starter.controllers', []).controller('ReceiptsCtrl', function($s
         left: 55
       },
       x(d) {
-        return d.key;
+        return d3.time.format('%m/%d')(new Date(d.key));
       },
       y(d) {
-        return d.y;
+        return d.total;
       },
       showValues: true,
+      objectequality: false,
       valueFormat(d) {
         return d3.format(',.2f')(d);
       },
@@ -89,19 +90,49 @@ angular.module('starter.controllers', []).controller('ReceiptsCtrl', function($s
     }
   ];
   $scope.getReceiptData = function() {
-    $http.get('http://ec2-18-220-68-160.us-east-2.compute.amazonaws.com:8000/receipts/users/1').then(function(res) {
-      res.data.forEach(function(receipt) {
-        const dataObj = {};
-        dataObj.key = receipt.date;
-        dataObj.y = receipt.items.map(function(item) {
-          return parseInt(item.price, 10)
-        }).reduce(function(a, b) {
-          return a + b
-        });
-        $scope.data[0].values.push(dataObj);
-      });
-      console.log($scope.data);
+    $http.get('http://ec2-18-220-68-160.us-east-2.compute.amazonaws.com:8001/receipts/users/1').then(function(res) {
+      $scope.receipts = res.data;
+      $scope.updateGraphData();
     });
   };
   $scope.getReceiptData();
+  // $scope.api.update();
+  $scope.updateGraphData = function() {
+    $scope.receipts.forEach(function(receipt) {
+      const dataObj = {};
+      dataObj.key = receipt.date;
+      dataObj.items = receipt.items;
+      dataObj.total = dataObj.items.map(function(item) {
+        return parseInt(item.price, 10)
+      }).reduce(function(a, b) {
+        return a + b
+      });
+      $scope.data[0].values.push(dataObj);
+    });
+  };
+
+  $ionicModal.fromTemplateUrl('templates/graphControl.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.showGraphControl = function(receipts) {
+    $scope.receipts = receipts;
+    $scope.modal.show();
+  };
+
+  $scope.saveGraphControls = function(controlForm) {
+    const activeItems = [];
+    $scope.receipts.items.filter((item) => {
+      if (activeItems.indexOf(item.id) !== -1) {
+        return item;
+      }
+    });
+    $scope.updateGraphData();
+  }
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  }
 });
